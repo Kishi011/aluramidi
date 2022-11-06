b5b95c7eca5934b5f65e3efc1c0d2752370ee6b4
Vinicius Mulinari Stuani <viniciusmulinari.st@gmail.com>
2022-11-06 12:30:01 -0300

    polimento e tratamento do código

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 function tocaSom(idAudio) {
-    let som = document.querySelector(idAudio);
-    som.play();
+    const som = document.querySelector(idAudio);
+    // tratamento de código
+    if(som.localName === 'audio' && som) {
+        som.play();
+    }
+    else {
+        console.log('Não é um elemento válido');
+    }
 }
 
 const lista_buttons = document.querySelectorAll('.tecla');
@@ -10,4 +16,14 @@ for (let i = 0; i < lista_buttons.length; i++) {
     lista_buttons[i].onclick = function () {
         tocaSom('#'+lista_sons[i].id);
     };
+    
+    lista_buttons[i].onkeydown = function (evento) {
+        if(evento.keyCode === 32 || evento.keyCode === 13){
+            lista_buttons[i].classList.add('ativa');
+        }
+    }
+
+    lista_buttons[i].onkeyup = function () {
+        lista_buttons[i].classList.remove('ativa');
+    }
 }
